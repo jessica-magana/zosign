8f5e6616c120ca802944f3a142f8a904961f08a9
Jessica <98279129+jessica-magana@users.noreply.github.com>
2022-04-21 14:44:52 -0600

    initialcommit

--- result.js
+++ result.js
@@ -27,13 +27,18 @@
         
             $totalCorrect = 0;
             
-            if ($answer1 == "C") { $totalCorrect++; }
-            if ($answer2 == "D") { $totalCorrect++; }
+            if ($answer1 == "A") { $totalCorrect++; }
+            if ($answer2 == "A") { $totalCorrect++; }
             if ($answer3 == "A") { $totalCorrect++; }
-            if ($answer4 == "B") { $totalCorrect++; }
+            if ($answer4 == "A") { $totalCorrect++; }
             if ($answer5 == "D") { $totalCorrect++; }
+            if ($answer6 == "C") { $totalCorrect++; }
+            if ($answer7 == "C") { $totalCorrect++; }
+            if ($answer8 == "D") { $totalCorrect++; }
+            if ($answer9 == "D") { $totalCorrect++; }
+            if ($answer10 == "C") { $totalCorrect++; }
             
-            echo "<div id='results'>$totalCorrect / 5 correct</div>";
+            echo "<div id='results'>Sagittarius</div>";
             
         ?>
  
